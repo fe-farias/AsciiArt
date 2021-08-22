d78a1563ff8e0f73f4c484a5ebfdf524ecb557d5
Felipe Farias <fefarias97@gmail.com>
2021-08-22 20:31:47 -0300

    Adiciona resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+
+
+        }
+    }
+}
